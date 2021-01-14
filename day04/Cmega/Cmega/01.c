c2ac1aef630e52d748aa62429f4999e42384e040
User seongbm <User@BCLASS-13>
2021-01-14 19:19:31 +0900

    대입 연산 연습

--- day04/Cmega/Cmega/01.c
+++ day04/Cmega/Cmega/01.c
@@ -2,5 +2,24 @@
 
 int main(void) {
 
+	int age;
+	age = 10;
+	
+	int age2 = 10;
+
+	int a = 10;
+	int b = 20;
+	int c  = 30;
+
+	int a, b, c;
+	a = 10;
+	b = 20;
+	c = 30;
+
+	int a_1 = 10, b_1 = 20, c_1 = 30;
+
+
+
+
 	return 0;
 }
