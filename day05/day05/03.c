f6d78c7ce8bed3deaf800c6ef3404b4dc40d2906
https://github.com/seongbm/c_mega.git <User@BCLASS-08>
2021-01-15 20:51:08 +0900

    scanf 는 \n이 안되요ㅠ

--- day05/day05/03.c
+++ day05/day05/03.c
@@ -5,11 +5,11 @@
 #include<stdio.h>
 int main(void) {
 	int q1;
-	printf("두 자의 숫자를 입력하세요:");
-	scanf("%d\n",q1);
+	printf("두 자리의 자연수를 입력하세요:");
+	scanf("%d",&q1); //scanf 는 \n이 안되요ㅠ
 	int  a1, a2;
 	a1 = q1 / 10;
 	a2 = q1 % 10;
-	printf("%d를 뒤집으면 %d%d입니다", q1,a1, a2);
+	printf("\n%d를 뒤집으면 %d%d입니다", q1,a1, a2);
 	return 0;
 }
